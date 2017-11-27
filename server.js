b2b016a405cb525c4c4cc60e721ac8e026a915b2
sai suryanarayanan <sai.sumughi@gmail.com>
2017-11-27 14:39:21 -0500

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+const port  = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// register middleware
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next(); // middleware shud call next, else code after this block will not get executed.
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.send('<h2>Welcome!</h2>');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
